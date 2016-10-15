3b27523f3f7d58985bb5a526de5171a63f96d44f
goyyapazham <nalanda99@gmail.com>
2016-10-15 14:07:26 -0400

    updated

--- db_builder.py
+++ db_builder.py
@@ -9,11 +9,22 @@ c = db.cursor()    #facilitate db ops
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 #...perhaps by beginning with these examples...
-q = "CREATE TABLE students (name TEXT, id INTEGER)"
-c.execute(q)    #run SQL query
 
-q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
+q = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)"
 c.execute(q)
+with open('peeps.csv') as csvfile:
+    pdata = csv.DictReader(csvfile)
+    for row in pdata:
+        q = "INSERT INTO students VALUES (\'%s\', %d, %d)" % (row['name'], int(row['age']), int(row['id']))
+        c.execute(q)   
+
+q = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)"
+c.execute(q)
+with open('courses.csv') as csvfile:
+    cdata = csv.DictReader(csvfile)
+    for row in cdata:
+        q = "INSERT INTO courses VALUES (\'%s\', %d, %d)" % (row['code'], int(row['mark']), int(row['id']))
+        c.execute(q)
 
 #==========================================================
 db.commit() #save changes
